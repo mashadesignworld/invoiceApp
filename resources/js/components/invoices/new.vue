5b3011f26890a1db69a7e2105e4216c25a33164c
mashadesignworld <njugunak.kevin@gmail.com>
2024-03-21 17:38:52 +0300

    fifth commit

--- resources/js/components/invoices/new.vue
+++ resources/js/components/invoices/new.vue
@@ -6,10 +6,16 @@ import { useRouter } from "vue-router"
 let form = ref([])
 let allcustomers = ref([])
 let customer_id = ref([])
+let item = ref([])
+let ListCart= ref([])
+const showModal = ref(false)
+const hideModal = ref(true)
+let Listproducts = ref([])
 
 onMounted(async () => {
     indexForm()
     getAllCustomers()
+    getProducts()
 })
 
 const indexForm = async () => {
@@ -26,7 +32,35 @@ const getAllCustomers = async () => {
     allcustomers.value = response.data.customers
 }
 
+const addCart = (item) => {
+    
+    
+    const itemcart = {
+        id : item.id,
+        item_code : item.item_code,
+        description : item.description,
+        unit_price : item.unit_price,
+        quantity : item.quantity,
+    }
+    ListCart.value.push(itemcart)
+    closeModel()
+}
 
+const openModel = () => {
+    showModal.value = !showModal.value
+}
+
+const closeModel = () => {
+    showModal.value = !hideModal.value
+}
+
+const getProducts = async () =>
+{
+
+    let response = await axios.get('/api/products')
+    console.log('products', response)
+    Listproducts.value = response.data.products
+}
 
 </script>
 <template>
@@ -49,20 +83,21 @@ const getAllCustomers = async () => {
                     <p class="my-1">Customer</p>
                     <select name="" id="" class="input" v-model="customer_id">
                         <option v-for="customer in allcustomers" :key="customer.d">
-                        {{customer.firstname}}/option>
+                        {{customer.firstname}}
+                        </option>
                     </select>
                 </div>
                 <div>
                     <p class="my-1">Date</p> 
-                    <input id="date" placeholder="dd-mm-yyyy" type="date" class="input"> <!---->
+                    <input id="date" placeholder="dd-mm-yyyy" type="date" class="input" v-model="form.date"> <!---->
                     <p class="my-1">Due Date</p> 
-                    <input id="due_date" type="date" class="input">
+                    <input id="due_date" type="date" class="input" v-model="form.due_date">
                 </div>
                 <div>
-                    <p class="my-1">Numero</p> 
-                    <input type="text" class="input"> 
+                    <p class="my-1">Number</p> 
+                    <input type="text" class="input" v-model="form.number"> 
                     <p class="my-1">Reference(Optional)</p> 
-                    <input type="text" class="input">
+                    <input type="text" class="input" v-model="form.reference">
                 </div>
             </div>
             <br><br>
@@ -77,23 +112,24 @@ const getAllCustomers = async () => {
                 </div>
     
                 <!-- item 1 -->
-                <div class="table--items2">
-                    <p>#093654 vjxhchkvhxc vkxckvjkxc jkvjxckvjkx </p>
+                <div class="table--items2" v-for="(itemcart, i) in ListCart" :key="itemcart.id">
+                    <p>#{{itemcart.item_code}} {{itemcart.description}}</p>
                     <p>
-                        <input type="text" class="input" >
+                        <input type="text" class="input" v-model="itemcart.unit_price">
                     </p>
                     <p>
-                        <input type="text" class="input" >
+                        <input type="text" class="input" v-model="itemcart.quantity" >
                     </p>
-                    <p>
-                        $ 10000
+                    <p v-if="itemcart.quantity">
+                        $ {{(itemcart.quantity)*(itemcart.unit_price)}}
                     </p>
+                    <p v-else></p>
                     <p style="color: red; font-size: 24px;cursor: pointer;">
                         &times;
                     </p>
                 </div>
                 <div style="padding: 10px 30px !important;">
-                    <button class="btn btn-sm btn__open--modal">Add New Line</button>
+                    <button class="btn btn-sm btn__open--modal" @click="openModel()">Add New Line</button>
                 </div>
             </div>
 
@@ -133,20 +169,36 @@ const getAllCustomers = async () => {
         
     </div>
     <!--==================== add modal items ====================-->
-    <div class="modal main__modal ">
+    <div class="modal main__modal " :class="{show: showModal}">
         <div class="modal__content">
-            <span class="modal__close btn__close--modal">×</span>
+            <span class="modal__close btn__close--modal" @click= "closeModel()">×</span>
             <h3 class="modal__title">Add Item</h3>
             <hr><br>
             <div class="modal__items">
-                <select class="input my-1">
-                    <option value="None">None</option>
-                    <option value="None">LBC Padala</option>
-                </select>
+                <ul style="list-style:none">
+                    <li v-for="(item, i) in Listproducts" :key="item.id"
+                    style="display:grid;grid-template-columns:
+                    30px 350px 15px;
+                    align-items:center;
+                    padding-bottom:5px;"
+                    >
+                    <p>{{i+1}}</p>
+                    <a href="#">{{item.item_code}} {{item.description}}</a>
+                    <button @click="addCart(item)"
+                    style="border:1px solid
+                    #e0e0e0;
+                    width:35px;height:35px;
+                    cursor:pointer;">
+                    +
+                    </button>
+                    </li>
+
+                </ul>
+                
             </div>
             <br><hr>
             <div class="model__footer">
-                <button class="btn btn-light mr-2 btn__close--modal">
+                <button class="btn btn-light mr-2 btn__close--modal" @click= "closeModel()">
                     Cancel
                 </button>
                 <button class="btn btn-light btn__close--modal ">Save</button>
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -31,3 +32,6 @@ Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
 
 
 Route::get('/customers', [CustomerController::class, 'all_customer']);
+
+
+Route::get('/products', [ProductController::class, 'all_product']);
